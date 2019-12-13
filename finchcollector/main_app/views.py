99c8b59ff1fdad412a14884d50acbdfab06c5cc8
BlaineShelton <mapleteer@gmail.com>
2019-12-13 14:00:40 -0800

    adds model and detail view

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -2,18 +2,7 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-class Finch:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-finches = [
-  Finch('Lolo', 'feathered', 'foul little demon', 3),
-  Finch('Sachi', 'featherless', 'he hasn''t hatched yet so ya know', 0),
-  Finch('Raven', 'not a finch', 'he is just a raven I didnt choose him he chose me', 4)
-]
+from .models import Finch
 
 # Create your views here.
 def home(request):
@@ -23,4 +12,9 @@ def about(request):
     return render(request,'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request,'finches/index.html', {'finches': finches})
+
+def finches_detail(request,finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request,'finches/detail.html', {'finch': finch})
