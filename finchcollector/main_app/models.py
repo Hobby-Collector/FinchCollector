37f96388247869ed2cccccd71d30ab0b2f1804d3
BlaineShelton <mapleteer@gmail.com>
2019-12-18 14:02:53 -0800

    add users login and logout

--- finchcollector/main_app/models.py
+++ finchcollector/main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
 MEALS = (
     ('B', 'Breakfast'),
@@ -8,11 +10,24 @@ MEALS = (
 )
 
 
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
+
+
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
+    toys = models.ManyToManyField(Toy)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -20,21 +35,34 @@ class Finch(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
 
+    def form_valid(self, form):
+            # Assign the logged in user (self.request.user)
+        form.instance.user = self.request.user  # form.instance is the cat
+        # Let the CreateView do its job as usual
+        return super().form_valid(form)
+
 
 class Feeding(models.Model):
-    date = models.DateField('feeding date')
+    date = models.DateField()
     meal = models.CharField(
         max_length=1,
-
-        # add the 'choices' field option
         choices=MEALS,
-
-        # set the default value for meal to be 'B'
         default=MEALS[0][0]
     )
 
-    #creates a finch_id foreign key
+    # creates a finch_id foreign key
     finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
+
+
+class Photo(models.Model):
+    url = models.CharField(max_length=200)
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for cat_id: {self.cat_id} @{self.url}"
--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,31 +1,137 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.http import HttpResponse
-from .models import Finch
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy, Photo
+from .forms import FeedingForm
+import uuid
+import boto3
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
-class FinchCreate(CreateView):
-    model = Finch
-    fields = '__all__'
 
-class FinchUpdate(UpdateView):
-    model = Finch
-    fields = ['breed','description','age']
 
-class FinchDelete(DeleteView):
+class FinchCreate(LoginRequiredMixin,CreateView):
     model = Finch
-    success_URL = '/finches/'
-    
+    fields = ['name', 'breed', 'description', 'age']
+
+
+class FinchUpdate(LoginRequiredMixin,UpdateView):
+    model = Finch
+    fields = ['breed', 'description', 'age']
+
+
+class FinchDelete(LoginRequiredMixin,DeleteView):
+    model = Finch
+    success_url = '/finches/'
+
+
 def home(request):
-    return render(request,'base.html')
+    return render(request, 'home.html')
+
 
 def about(request):
-    return render(request,'about.html')
+    return render(request, 'about.html')
 
+@login_required
 def finches_index(request):
-    finches = Finch.objects.all()
-    return render(request,'finches/index.html', {'finches': finches})
+    finches = Finch.objects.filter(user=request.user)
+    return render(request, 'finches/index.html', {'finches': finches})
 
-def finches_detail(request,finch_id):
+@login_required
+def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request,'finches/detail.html', {'finch': finch})
+    toys_finch_doesnt_have = Toy.objects.exclude(
+        id__in=finch.toys.all().values_list('id'))
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', {
+        'finch': finch, 'feeding_form': feeding_form, 'toys': toys_finch_doesnt_have})
+
+@login_required
+def add_feeding(request, finch_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+        return redirect('detail', finch_id=finch_id)
+
+@login_required
+def assoc_toy(request, finch_id, toy_id):
+    # Note that you can pass a toy's id instead of the whole object
+    Finch.objects.get(id=finch_id).toys.add(toy_id)
+    return redirect('detail', finch_id=finch_id)
+
+@login_required
+def unassoc_toy(request, finch_id, toy_id):
+    Finch.objects.get(id=finch_id).toys.remove(toy_id)
+    return redirect('detail', finch_id=finch_id)
+
+
+class ToyList(LoginRequiredMixin,ListView):
+    model = Toy
+
+
+class ToyDetail(LoginRequiredMixin,DetailView):
+    model = Toy
+
+
+class ToyCreate(LoginRequiredMixin,CreateView):
+    model = Toy
+    fields = '__all__'
+
+
+class ToyUpdate(LoginRequiredMixin,UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+
+class ToyDelete(LoginRequiredMixin,DeleteView):
+    model = Toy
+    success_url = '/toys/'
+
+@login_required
+def add_photo(request, finch_id):
+    S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
+    BUCKET = '#'
+
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + \
+            photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            # we can assign to finch_id or cat (if you have a cat object)
+            photo = Photo(url=url, finch_id=finch_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail', finch_id=finch_id)
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in via code
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
